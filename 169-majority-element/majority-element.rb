252cb9dc30ae52e3cde7b25868267bcd18ee3853
Rashika Keshwan <145734903+rkeshwan@users.noreply.github.com>
2024-05-05 13:49:11 +1000

    Time: 74 ms (71.02%) | Memory: 213.5 MB (11.22%) - LeetSync

--- 169-majority-element/majority-element.rb
+++ 169-majority-element/majority-element.rb
@@ -3,15 +3,16 @@
 def majority_element(nums)
     # Calculate size of nums array
     n = nums.length
+    # Create new array with only unique elements from nums array
     array = nums.uniq
     # Store all elements from nums array in new array that have more than 50% occurence
     new = []
-    #Iterate over each element in nums array
+    #Iterate over each unique element from nums array only once to reduce run time
     array.each do |num|
         # Find the element that makes up more than 50% of the size of nums array
         if nums.count(num) > (n/2)
             new << num
         end
     end
-    new.uniq.first
+    new.first
 end
